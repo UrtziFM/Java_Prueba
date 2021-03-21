068f65268bb6ca7e71e3b120546b3b5832fa2ea6
u100247 <u100247@MM0012B7.local>
2021-03-21 19:22:19 +0100

    First Commit

---
+++ App.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.lang.Math;
+/** Esto es un comentario para probar el javadoc
+* este texto aparecerá en el archivo HTML generado.
+* <strong>Realizado en agosto 2021</strong>
+*
+* @author Urtzi Arana
+* @version 1.0
+*/
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Mi primer programa");
+        String texto =JOptionPane.showInputDialog("Escriba un número entero");
+        int n = Integer.parseInt(texto);
+        double x = Math.pow(n,3);
+        System.out.println(x);
+    }
+}
