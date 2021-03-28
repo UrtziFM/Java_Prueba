c2b0e6710f8809ee5f7c59115cb5217ecf078639
u100247 <u100247@MM0012B7.local>
2021-03-28 13:36:45 +0200

    playing with constructors and conditionals

---
+++ Company.java
@@ -0,0 +1,13 @@
+public class Company {
+
+    public static void main(String[] args) throws Exception {
+        Profile profile1 = new Profile("Jon", 40, "Bilbao");
+        Profile profile2 = new Profile("Maider", 39, "Basauri");
+        profile1.getProfile();
+        profile2.getProfile();
+        do {
+            System.out.println("Hello World");
+        } while(profile1.employeeAge == 39);
+    }
+    
+}
---
+++ Profile.java
@@ -0,0 +1,18 @@
+public class Profile {
+    private String employeeName;
+    public int employeeAge;
+    public String employeeCity;
+
+    public Profile(String name, int age, String city) {
+        employeeName = name;
+        employeeAge = age;
+        employeeCity = city;
+    }
+
+    public void getProfile() {
+        System.out.println("This is " + employeeName);
+        System.out.println("It's " + employeeAge);
+        System.out.println("And lives in " + employeeCity);
+    }
+}
+
